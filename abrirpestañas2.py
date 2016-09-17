c57474d70b8022fb0cde168270ed74b8fcbfbe30
RafaelAybar <rafaelaybar@hotmail.com>
2016-09-17 15:37:56 +0200

    Abrir páginas web en una sola pestaña
    He estado trasteando con el anterior ejemplo y quiero que las páginas web del script se abran en una única pestaña, pero no se exactamente por qué no funciona.

---
+++ abrirpestañas2.py
@@ -0,0 +1,13 @@
+import os
+import webbrowser
+webbrowser.open_new("https://es5.forgeofempires.com/game/index?ref=")
+webbrowser.open_new_tab("http://www.elalmeria.es/")
+webbrowser.open_new_tab("http://www.ideal.es/almeria/")
+webbrowser.open_new_tab("http://www.eldiario.es/")
+webbrowser.open_new_tab("http://www.elconfidencial.com/")
+webbrowser.open_new_tab("http://www.20minutos.es/")
+webbrowser.open_new_tab("http://vozpopuli.com/inicio")
+webbrowser.open_new_tab("http://www.elplural.com/")
+webbrowser.open_new_tab("http://www.infolibre.es/")
+webbrowser.open_new_tab("https://www.google.es/?client=firefox-b#q=rafa+es+genial&gfe_rd=cr")
+#quiero estas pestañas se abran en un una sola ventana.
